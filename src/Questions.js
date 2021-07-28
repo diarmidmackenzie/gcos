6575867bf5e88ceb15ee37e255ef4593afd271fa
diarmidmackenzie <diarmid.mackenzie@gmail.com>
2021-07-28 19:57:24 +0100

    Improve presentation

--- src/Questions.js
+++ src/Questions.js
@@ -9,9 +9,14 @@ export function GcosIntro(props) {
 
   return (
     <>
+      <p> This page provides a self-assessment of your General Causality Orientation.</p>
       <p> To Begin the Survey Press the Start button </p>
       <button type="button"
               onClick = {() => props.moveCursor(1)}>Start</button>
+      <p> Some notes about privacy...</p>
+      <p> (No data is collected.  No data leaves the browser.)</p>
+      <p> Some notes about how the survey works...</p>
+      <p> (choose a 'likelihood' for each possible response)</p>
     </>
   )
 }
@@ -22,6 +27,10 @@ export function GcosQuestions(props) {
   var nextActive = ((props.scores[props.cursor][0] > 0 ) &&
                     (props.scores[props.cursor][1] > 0 ) &&
                     (props.scores[props.cursor][2] > 0 ))
+
+  var nextText = (props.cursor === (props.questions.length - 1)) ?
+                 "Finish" : "Next"
+
   return (
     <>
       <Situation
@@ -34,7 +43,7 @@ export function GcosQuestions(props) {
               onClick = {() => props.moveCursor(-1)}>Prev</button>
       <button type="button"
               onClick = {() => props.moveCursor(1)}
-              disabled = {nextActive ? null : true}>Next</button>
+              disabled = {nextActive ? null : true}>{nextText}</button>
     </>
   )
 };
@@ -49,7 +58,9 @@ function Situation(props) {
   return (
     <>
       <div>
-        {props.question}
+        <p className="number">{(props.index + 1) + "/" + props.questions.length + " "} </p>
+        <p className="question">{props.question}</p>
+        <br/>
       </div>
       <div>
         {props.scoreGroup.map((score, index) => (
@@ -73,11 +84,15 @@ function Situation(props) {
 // setScore = callback to set a score
 // selectedValue = which value (if any) is checked.
 function Response(props) {
+
+  const prepends = ["a) ", "b) ", "c) "];
+
   return (
     <>
-      <div>
-        {props.responseText}
+      <div className = "response">
+        {prepends[props.index] + props.responseText}
       </div>
+      <div className = "radio-group">
       {
         Array.from(Array(7).keys()).map((item) => (
           <Option
@@ -90,6 +105,16 @@ function Response(props) {
           />
         ))
       }
+      </div>
+      <div className = "radio-key">
+        very unlikely
+      </div>
+      <div className = "radio-key">
+        moderately likely
+      </div>
+      <div className = "radio-key">
+        very likely
+      </div>
     </>
   )
 }
@@ -103,13 +128,14 @@ function Option(props) {
 
   return(
     <>
-      <input type = "radio"
-             name = {props.groupIndex + "-" + props.index}
-             value = {props.value}
-             defaultChecked = {props.defaultChecked ? "checked" : null}
-             onClick = {() => props.setScore(props.groupIndex, props.index, props.value)}/>
-      <label htmlFor={props.value}>{props.value}</label>
+      <label htmlFor={props.value}>
+        <input type = "radio"
+               name = {props.groupIndex + "-" + props.index}
+               value = {props.value}
+               defaultChecked = {props.defaultChecked ? "checked" : null}
+               onClick = {() => props.setScore(props.groupIndex, props.index, props.value)}/>
+        {props.value}
+      </label>
     </>
-
   )
 }
